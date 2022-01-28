c3554636684243159fcd76e446c763ff6c826715
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-01-28 04:09:47 +0330

    CRD request clean code

--- controller/request.js
+++ controller/request.js
@@ -1,4 +1,8 @@
 const requestModel = require("../model/request")
+const taskModel = require("../model/task")
+const repairmanModel = require("../model/repairman")
+
+
 
 async function addRequest(req, res) {
     try {
@@ -15,5 +19,88 @@ async function addRequest(req, res) {
 
 
 
+async function getAllRequests(req, res) {
+    try {
+        const allRequests = await requestModel.findAll()
+        if (allRequests.length == 0) {
+            return res.send("there is no request added")
+        }
+        res.send(allRequests)
+    } catch (error) {
+        res.send(error)
+    }
+}
 
-module.exports = { addRequest }
+
+
+async function deleteRequest(req, res) {
+    try {
+        const { request_id } = req.body
+        const result = await requestModel.destroy({
+            where: {
+                request_id
+            }
+        })
+        if (result == 1) {
+            res.send("deleted")
+        }
+        else {
+            res.send(`couldnt find request by this id : ${request_id}   !`)
+        }
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+
+
+
+
+async function acceptRequest(req, res) {
+    try {
+        const { request_id, repairman_id } = req.body
+        const requestInformation = (await requestModel.findByPk(request_id)).dataValues
+        if (!requestInformation) {
+            return res.send("counlednt find request by this id ")
+        }
+
+        const result = await repairmanModel.findByPk(repairman_id, { attributes: ['skill'] })
+
+        if (!result) {
+            return res.send("repairman not found ")
+        }
+
+        const repairman = result.dataValues
+
+
+        if (repairman.skill !== requestInformation.skill) {
+            return res.send("repairman skill  does not your request skill")
+        }
+
+
+        const payload = {
+            repairman_id, admin_id: req.id, user_id: requestInformation.user_id,
+            subject: requestInformation.subject, message: requestInformation.message,
+            skill: requestInformation.skill
+        }
+
+        const resCreate = await taskModel.create(payload)
+        if (resCreate) {
+            return res.send(resCreate)
+        }
+        else {
+            res.send("create task faild")
+        }
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+
+
+module.exports = {
+    addRequest,
+    getAllRequests,
+    deleteRequest,
+    acceptRequest
+}
--- router/admin.js
+++ router/admin.js
@@ -6,10 +6,15 @@ const verifyToken = require("../middleware/verifyToken")
 
 // controller
 const controller = require("../controller/admin")
+const requestController = require("../controller/request")
+
 
 app.post("/signup", controller.signup)
 app.post("/login", controller.login)
-// app.post("/request", verifyToken, addRequest)
+
+app.get("/request", verifyToken, requestController.getAllRequests)
+app.delete("/request", verifyToken, requestController.deleteRequest)
+app.put("/request", verifyToken, requestController.acceptRequest)
 
 
 module.exports = app
